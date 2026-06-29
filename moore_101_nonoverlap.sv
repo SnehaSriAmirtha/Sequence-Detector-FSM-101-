611eb142af6cd8dcce8f0ad4971a852f351e54ed
Sneha Sri Amirtha <100421141+SnehaSriAmirtha@users.noreply.github.com>
2026-06-29 08:19:33 +0530

    Create moore_101_nonoverlap.sv

---
+++ moore_101_nonoverlap.sv
@@ -0,0 +1,61 @@
+
+module moore_101_nonoverlap (
+    input  wire clk,
+    input  wire reset,
+    input  wire in,
+    output reg  out
+);
+
+    
+    typedef enum reg [1:0] {
+        S0 = 2'b00,  
+        S1 = 2'b01, 
+        S2 = 2'b10,  
+        S3 = 2'b11   
+    } state_t;
+
+    state_t current_state, next_state;
+
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            current_state <= S0;
+        else
+            current_state <= next_state;
+    end
+
+
+    always @(*) begin
+        case (current_state)
+            S0: begin
+                if (in) next_state = S1;   
+                else    next_state = S0;
+            end
+
+            S1: begin
+                if (in) next_state = S1;  
+                else    next_state = S2;   
+            end
+
+            S2: begin
+                if (in) next_state = S3;   
+                else    next_state = S0;
+            end
+
+            S3: begin
+                
+                next_state = S0;
+            end
+
+            default: next_state = S0;
+        endcase
+    end
+
+  
+    always @(*) begin
+        case (current_state)
+            S3: out = 1'b1;  
+            default: out = 1'b0;
+        endcase
+    end
+  
+endmodule
